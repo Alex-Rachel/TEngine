9bc19f6aa48f4416fdbf7211f17e495184591ed3
Wiesen <vv_gool@163.com>
2026-06-11 16:06:52 +0800

    feat(uitk): add UITKAnimationType enum and IUITKAnimationDriver interface

---
+++ UnityProject/Assets/GameScripts/HotFix/GameLogic/Module/UITKModule/Animation/UITKAnimationType.cs
@@ -0,0 +1,18 @@
+namespace GameLogic
+{
+    /// <summary>
+    /// 内置动画预设类型。
+    /// </summary>
+    public enum UITKAnimationType
+    {
+        None,
+        FadeIn,
+        FadeOut,
+        SlideFromBottom,
+        SlideToBottom,
+        SlideFromRight,
+        SlideToRight,
+        ScaleIn,
+        ScaleOut,
+    }
+}
